48edb479848ae239f25706662c1d98084804e65e
GIT-CHEA002 <chhunsokchea317@gmail.com>
2026-06-07 01:00:22 +0700

    fixed index file

--- service/server.js
+++ service/server.js
@@ -1,9 +1,8 @@
-
 const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("service/jobs.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: false });
 
 const PORT = process.env.PORT || 5000;
 
